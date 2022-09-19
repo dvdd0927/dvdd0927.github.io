44b97ec0049f1724371ec8c6e1a40f1f3c1933ee
David Villanueva <dvillanueva@vertere-gs.com>
2022-09-19 14:37:43 +0800

    Add stretch goal session 12

---
+++ stretch-goal-s12/index.js
@@ -0,0 +1,52 @@
+// For Number 1
+const firstName = "John", lastName = "Smith", age = 30;
+
+const hobbies = ["Biking", "Mountain Climbing", "Swimming"];
+
+const workAddress = {
+	houseNumber: 32,
+	street: "Washington",
+	city: "Lincoln",
+	state: "Nebraska"
+};
+
+console.log("First Name: " + firstName);
+
+console.log("Last Name: " + lastName);
+
+console.log("Age: " + age);
+
+console.log("Hobbies: ");
+console.log(hobbies);
+
+console.log("Work Address:");
+console.log(workAddress);
+
+// For number 2
+
+const friends = ["Tony", "Bruce", "Thor", "Natasha", "Clint", "Nick"];
+
+const profile = {
+	username: "captain_america",
+	fullName: "Steve Rogers",
+	age: 40,
+	isActive: false
+};
+
+const bestFriend = "Bucky Barnes";
+const foundIn = "Artic Ocean";
+
+console.log("My full name is: " + profile.fullName);
+
+console.log("My current age is: " + profile.age);
+
+console.log("My Friends are:");
+console.log(friends);
+
+console.log("My Full Profile: ");
+console.log(profile);
+
+console.log("My bestfriend is: " + bestFriend );
+
+console.log("I was found frozen in: " + foundIn);
+
